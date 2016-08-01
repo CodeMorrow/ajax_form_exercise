6228efa1036145f720e92f3a76589ab458c379d1
John Morrow <CodeMorrow@yahoo.com>
2016-08-01 11:41:55 -0500

    created form, started JS

---
+++ global.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", function() {
+	
+	var form = new XMLHttpRequest();
+
+	var next = document.getElementsByClassName("nextButton");
+
+	next.addEventListener('click', function(){
+
+		
+
+	});
+
+});

55c4309814a5cd7096f9acde5ea13985f47aa5c3
John Morrow <CodeMorrow@yahoo.com>
2016-08-02 11:57:44 -0500

    cant add third member of band, add button doesnt function.

--- global.js
+++ global.js
@@ -6,24 +6,16 @@ window.addEventListener("load", function() {
 	var bandName = document.getElementById("bandName");
 	var bandMembers__info = document.getElementById("bandMembers__info");
 	var back = document.getElementsByClassName("backButton");
-	var addBandMember = document.getElementsByClassName("addBandMember");
+	var addBandMember = document.getElementById("addBandMember");
+	var bandMemberBackButton = document.getElementById("bandMemberBackButton");
+	var albumArtist = document.getElementById("albumArtist");
 
 	next[0].addEventListener('click', function(){
-
-		var albumArtist = document.getElementById("albumArtist");
-		var bandMemberBackButton = document.getElementById("bandMemberBackButton");
 
 		album.style.display = "none";
 		bandMembers.style.display ="block";
 		bandMemberBackButton.style.display = "none";
 		bandName.innerHTML = "Add " + albumArtist.value +"'s Members";
-
-		back[0].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
-
-
 
 	});
 
@@ -32,6 +24,53 @@ window.addEventListener("load", function() {
 		bandMembers.style.display = "none";
 		bandMembers__info.style.display = "block";
 
+	});
+
+	back[0].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	back[1].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	back[2].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	back[3].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	back[4].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	back[5].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	back[6].addEventListener('click', function(){
+			album.style.display = "block";
+			bandMembers.style.display ="none";
+		});
+
+	addBandMember.addEventListener('click', function(){
+		var i = 1;
+		i++;
+		var newBandMember = document.createElement('div');
+		newBandMember.setAttribute("id", "bandMember"+i+"")
+		newBandMember.innerHTML = '<input type="text" id="bandMemberName'+i+'" name="bandMember" placeholder="Name"><input type="text" id="bandMember__insturment'+i+'" name="bandMemberInsturment" placeholder="Insturment(s)"><input type="button" id="addBandMember" value="+"><p>Seperate multiple insturments with a comma.</p>';
+		bandMembers.appendChild(newBandMember)
+
+		var previousMember = document.getElementById('bandMember'+(i-1)+'')
+		previousMember.style.display = "none";
 	});
 
 });

e8385f3aa8fd5ae996ac950d16b92831744274ea
John Morrow <CodeMorrow@yahoo.com>
2016-08-02 14:27:27 -0500

    Navigating through form works.

--- global.js
+++ global.js
@@ -1,16 +1,29 @@
 window.addEventListener("load", function() {
 
-	var next = document.getElementsByClassName("nextButton");
-	var album = document.getElementById("album");
-	var bandMembers = document.getElementById("bandMembers");
-	var bandName = document.getElementById("bandName");
-	var bandMembers__info = document.getElementById("bandMembers__info");
-	var back = document.getElementsByClassName("backButton");
 	var addBandMember = document.getElementById("addBandMember");
-	var bandMemberBackButton = document.getElementById("bandMemberBackButton");
+	var album = document.getElementById("album");
 	var albumArtist = document.getElementById("albumArtist");
+	var albumImages = document.getElementById("albumImages");
+	var albumImages__backButton = document.getElementById("albumImages__backButton");
+	var albumInfo = document.getElementById("albumInfo");
+	var albumTracks = document.getElementById("albumTracks");
+	var albumTracks__backButton = document.getElementById("albumTracks__backButton");
+	var backButton = document.getElementsByClassName("backButton");
+	var bandMember__location = document.getElementById("bandMember__location");
+	var bandMemberBackButton = document.getElementById("bandMemberBackButton");
+	var bandMemberNames = document.getElementById("bandMemberNames");
+	var bandMembers = document.getElementById("bandMembers");
+	var bandMembers__info = document.getElementById("bandMembers__info");
+	var bandName = document.getElementById("bandName");
+	var confirmation = document.getElementById("confirmation");
+	var homeButton = document.getElementById("homeButton")
+	var nextButton = document.getElementsByClassName("nextButton");
+	var submitButton = document.getElementById("submit");
 
-	next[0].addEventListener('click', function(){
+
+// ############### Code of all my Next Buttons ##################
+
+	nextButton[0].addEventListener('click', function(){
 
 		album.style.display = "none";
 		bandMembers.style.display ="block";
@@ -19,57 +32,87 @@ window.addEventListener("load", function() {
 
 	});
 
-	next[1].addEventListener('click', function(){
+	nextButton[1].addEventListener('click', function(){
 
-		bandMembers.style.display = "none";
+		bandMembers.style.display ="none";
 		bandMembers__info.style.display = "block";
+		bandMember__location.innerHTML = "Add " + bandMemberName1 +"'s location";
+	});
+
+	nextButton[2].addEventListener('click', function(){
+
+		bandMembers__info.style.display ="none";
+		albumImages.style.display = "block";
+
+		albumImages__backButton.style.display = "none"
 
 	});
 
-	back[0].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+	nextButton[3].addEventListener('click', function(){
 
-	back[1].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+		albumImages.style.display = "none";
+		albumInfo.style.display = "block";
 
-	back[2].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+	});
 
-	back[3].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+	nextButton[4].addEventListener('click', function(){
 
-	back[4].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+		albumInfo.style.display = "none";
+		albumTracks.style.display = "block";
 
-	back[5].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+		albumTracks__backButton.style.display = "none";
 
-	back[6].addEventListener('click', function(){
-			album.style.display = "block";
-			bandMembers.style.display ="none";
-		});
+	});
+
+	submitButton.addEventListener('click', function(){
+
+		albumTracks.style.display = "none";
+		confirmation.style.display = "block";
+
+	});
+
+// ############### Code of all my Back Buttons ##################
+
+	backButton[0].addEventListener('click', function(){
+		album.style.display = "block";
+		bandMembers.style.display ="none";
+	});
+
+	backButton[1].addEventListener('click', function(){
+		bandMembers.style.display ="block";
+		bandMembers__info.style.display = "none";
+	});
+
+	backButton[2].addEventListener('click', function(){
+		bandMembers__info.style.display ="block";
+		albumImages.style.display = "none";
+	});
+
+	backButton[3].addEventListener('click', function(){
+		albumImages.style.display = "block";
+		albumInfo.style.display = "none";
+	});
+
+	backButton[4].addEventListener('click', function(){
+		albumInfo.style.display = "block";
+		albumTracks.style.display = "none";
+
+	});
+
+// ############### Code for Add Band Member Code ############## -WIP
 
 	addBandMember.addEventListener('click', function(){
+debugger;
 		var i = 1;
 		i++;
 		var newBandMember = document.createElement('div');
-		newBandMember.setAttribute("id", "bandMember"+i+"")
-		newBandMember.innerHTML = '<input type="text" id="bandMemberName'+i+'" name="bandMember" placeholder="Name"><input type="text" id="bandMember__insturment'+i+'" name="bandMemberInsturment" placeholder="Insturment(s)"><input type="button" id="addBandMember" value="+"><p>Seperate multiple insturments with a comma.</p>';
-		bandMembers.appendChild(newBandMember)
+		newBandMember.setAttribute("id", "bandMember"+i+"");
+		newBandMember.innerHTML = '<input type="text" id="bandMemberName'+i+'" name="bandMember" placeholder="Name"><input type="text" id="bandMember__insturment'+i+'" name="bandMemberInsturment" placeholder="Insturment(s)"><p>Seperate multiple insturments with a comma.</p>';
+		bandMemberNames.appendChild(newBandMember);
 
-		var previousMember = document.getElementById('bandMember'+(i-1)+'')
+		bandMemberBackButton.style.display = "block";
+
+		var previousMember = document.getElementById('bandMember'+(i-1));
 		previousMember.style.display = "none";
 	});
 
